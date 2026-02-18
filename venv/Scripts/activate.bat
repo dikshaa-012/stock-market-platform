07c0a2f9d5f5c4a95695c0a3a023ddabde302add
Diksha <dikshamalik012@gmail.com>
2026-02-19 01:05:26 +0530

    update

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\diksh\OneDrive\Desktop\New folder\stock-market-platform\venv
+set VIRTUAL_ENV=C:\Users\diksh\OneDrive\Desktop\projects\stock\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
